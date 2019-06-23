1b799df44cfe5f2ff9e7f181f667a52ef682b9bc
zakaria berkane <berkanezakaria@gmail.com>
2019-06-23 16:37:12 +0200

    please work

--- scraperfork.js
+++ scraperfork.js
@@ -33,7 +33,7 @@ const commandLineOptions = (command) => {
 			console.log("scraping youtube");
 			break;
 		case "scrape instagram":
-			console.log("scraping youtube");
+			dataWorker();
 			break;
 	}
 }
@@ -108,104 +108,112 @@ const instagramScraper = async (instagramUrl,id) => {
 }
 
 
-(async () => {
+// (async () => {
 
-	const data = await artists.findAll({
-		raw:true
+// 	const data = await artists.findAll({
+// 		raw:true
+// 	});
+
+	
+
+
+	
+
+
+// 	for(let i = 0; i < data.length; i++) {
+
+
+// 		//instagramScraper(data[i].instagram,data[i].id);
+
+// 		//console.log(data[i].id);
+// 		// const browser = await puppeteer.launch();
+// 		// const page = await browser.newPage();
+// 		// await page.goto(data[i].instagram);
+
+// 		// const scrapeData = await page.evaluate(() => {
+			
+// 		// 	try{
+// 		// 		return _sharedData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
+// 		// 	}catch(error) {
+// 		// 		console.error(error);
+// 		// 	};
+			
+		
+
+// 		// })
+// 		// await browser.close();
+
+// 		// console.log(scrapeData);
+// 		// if(scrapeData) {
+// 		// 	const scrapy = scrapeData.map(edgeSanitationWorker);
+// 		// 	const res = await Promise.all(scrapy);
+			
+// 		// 	let id = data[i].id;
+
+
+
+// 		// 	res.map(i => {
+// 		// 		instagramPosts.create({
+// 		// 			artistId: id,
+// 		// 			shortCode: i.shortcode,
+// 		// 			html: i.html,
+// 		// 			timestamp: i.timestamp,
+// 		// 			likes: i.likes
+// 		// 		})
+// 		// 	})
+// 		// }
+		
+// 		// const wikiText = await Promise.resolve(wikiScraper(data[i].wikipedia));
+		
+// 		// wikiModel.create({
+// 		// 	artistId: data[i].id,
+// 		// 	wiki_description: wikiText
+// 		// });
+
+// 		// setTimeout(function(){ticketMasterScraper(data[i].name)},1000*data[i].id)
+
+
+
+// 		// let youtubePromises = null;
+
+
+// 		// if(data[i].youtube.indexOf("channel") >= 0) {
+// 		// 	const identifier = getYoutubeIdentifier(data[i].youtube);
+// 		// 	youtubePromises = youtubeScraper(identifier,false);
+// 		// }else if(data[i].youtube.indexOf("user") >= 0){
+// 		// 	const identifier = getYoutubeIdentifier(data[i].youtube);
+// 		// 	youtubePromises = youtubeScraper(false,identifier)
+// 		// }
+
+// 		// const resolvedPromises = await Promise.resolve(youtubePromises);
+
+
+// 		// resolvedPromises.forEach(video => {
+// 		// 	console.log(video)
+// 		// 	youtubeModel.create({
+// 		// 		artistId: data[i].id,
+// 		// 		video_id: video.videoId,
+// 		// 		post_date: video.publishDate
+// 		// 	});
+// 		// });
+// 	}
+	
+
+
+	
+
+
+
+
+
+
+
+// })();
+
+const dataWorker = async (socialmediaScraperFunction) => {
+	const artistData = await artists.findAll();
+
+	artists.forEach((artist, index) => {
+		console.log(artist,index);
 	});
-
-	
-
-
-	
-
-
-	for(let i = 0; i < data.length; i++) {
-
-
-		//instagramScraper(data[i].instagram,data[i].id);
-
-		//console.log(data[i].id);
-		// const browser = await puppeteer.launch();
-		// const page = await browser.newPage();
-		// await page.goto(data[i].instagram);
-
-		// const scrapeData = await page.evaluate(() => {
-			
-		// 	try{
-		// 		return _sharedData.entry_data.ProfilePage[0].graphql.user.edge_owner_to_timeline_media.edges
-		// 	}catch(error) {
-		// 		console.error(error);
-		// 	};
-			
-		
-
-		// })
-		// await browser.close();
-
-		// console.log(scrapeData);
-		// if(scrapeData) {
-		// 	const scrapy = scrapeData.map(edgeSanitationWorker);
-		// 	const res = await Promise.all(scrapy);
-			
-		// 	let id = data[i].id;
-
-
-
-		// 	res.map(i => {
-		// 		instagramPosts.create({
-		// 			artistId: id,
-		// 			shortCode: i.shortcode,
-		// 			html: i.html,
-		// 			timestamp: i.timestamp,
-		// 			likes: i.likes
-		// 		})
-		// 	})
-		// }
-		
-		// const wikiText = await Promise.resolve(wikiScraper(data[i].wikipedia));
-		
-		// wikiModel.create({
-		// 	artistId: data[i].id,
-		// 	wiki_description: wikiText
-		// });
-
-		// setTimeout(function(){ticketMasterScraper(data[i].name)},1000*data[i].id)
-
-
-
-		// let youtubePromises = null;
-
-
-		// if(data[i].youtube.indexOf("channel") >= 0) {
-		// 	const identifier = getYoutubeIdentifier(data[i].youtube);
-		// 	youtubePromises = youtubeScraper(identifier,false);
-		// }else if(data[i].youtube.indexOf("user") >= 0){
-		// 	const identifier = getYoutubeIdentifier(data[i].youtube);
-		// 	youtubePromises = youtubeScraper(false,identifier)
-		// }
-
-		// const resolvedPromises = await Promise.resolve(youtubePromises);
-
-
-		// resolvedPromises.forEach(video => {
-		// 	console.log(video)
-		// 	youtubeModel.create({
-		// 		artistId: data[i].id,
-		// 		video_id: video.videoId,
-		// 		post_date: video.publishDate
-		// 	});
-		// });
-	}
-	
-
-
-	
-
-
-
-
-
-
-
-})();
+};
